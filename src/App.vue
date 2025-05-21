012391b03e47ae44c485b1d2181cfd4e911d90a8
Eivind Simonsen <74544541+eivindsimonsen@users.noreply.github.com>
2025-05-21 12:45:46 +0200

    Added all cards as json

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,47 @@
 <script setup lang="ts">
+import PreGame from "./components/PreGame.vue";
+
 </script>
 
 <template>
- <h1>Gwent Card Game</h1>
+  <main>
+    <!-- <h1>Gwent Card Game</h1> -->
+     <PreGame />
+     <div class="opponent-sentry">awdawd</div>
+     <div class="opponent-length">awdawd</div>
+     <div class="opponent-close">awdawd</div>
+     <div class="player-sentry">awdawd</div>
+     <div class="player-length">awdawd</div>
+     <div class="player-close">awdawd</div>
+  </main>
 </template>
 
-<style scoped>
+<style>
+* {
+  box-sizing: border-box;
+}
 
+h1 {
+  color: white;
+}
+
+body {
+  font-family: var(--font-family);
+  margin: 0;
+  padding: 0;
+  background-color: var(--background-color);
+  min-height: 100vh;
+  background-image: var(--background-image);
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+
+main {
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  grid-template-rows: repeat(6, 1fr);
+  /*border: 1px solid red; /* TODO: Remove border */
+  z-index: 20;
+}
 </style>
